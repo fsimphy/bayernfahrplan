04dbb1f6d94cc623f544e7686df6120ce38de262
Oliver Rümpelein <oli_r@fg4f.de>
2018-03-30 22:29:55 +0200

    Generified function, added lower level

--- source/fahrplanparser/parser.d
+++ source/fahrplanparser/parser.d
@@ -32,15 +32,76 @@ static this()
 
 public:
 
-auto getFirstSubnodeWithName(DOMEntity!string dom, string subnodeName)
+auto getSubnodesWithName(T)(DOMEntity!T dom, string subnodeName)
+in
+{
+    assert(subnodeName != "");
+}
+do {
+    return dom.children
+        .filter!(node => node.name == subnodeName);
+}
+
+@system
+{
+    unittest
+    {
+        auto nodes =
+        (
+        // dfmt off
+        "<?xml version='1.0' encoding='UTF-8'?>\n" ~
+        "<n1>node1</n1>"
+        // dfmt.on
+        ).parseDOM.getSubnodesWithName("n1");
+        
+        nodes.empty.should.equal(false);
+        nodes.front.name.should.equal("n1");
+        nodes.popFront;
+        nodes.empty.should.equal(true);
+    }
+
+    unittest
+    {
+        auto nodes =
+        (
+        // dfmt off
+        "<?xml version='1.0' encoding='UTF-8'?>\n" ~
+        "<n1>\n" ~
+        "   <n2>text1</n2>\n" ~
+        "   <n2>text2</n2>\n" ~
+        "</n1>"
+        // dfmt.on
+        ).parseDOM.getSubnodesWithName("n1")
+        .front
+        .getSubnodesWithName("n2");
+        
+        nodes.empty.should.equal(false);
+        auto node2_1 = nodes.front;
+        node2_1.name.should.equal("n2");
+        node2_1.children.front.text.should.equal("text1");
+
+        nodes.popFront;
+        auto node2_2 = nodes.front;
+        node2_2.name.should.equal("n2");
+        node2_2.children.front.text.should.equal("text2");
+
+        nodes.popFront;
+        nodes.empty.should.equal(true);
+    }
+}
+
+/**
+ * Fetches the first (direct) subnode with a given name.
+ * If none is found, throws a CouldNotFindNodeWithContentException.
+ */
+auto getFirstSubnodeWithName(T)(DOMEntity!T dom, string subnodeName)
 in
 {
     assert(subnodeName != "");
 }
 do
 {
-    auto childs = dom.children
-        .filter!(node => node.name == subnodeName);
+    auto childs = dom.getSubnodesWithName(subnodeName);
     if (childs.empty) {
         throw new CouldNotFindNodeWithContentException(subnodeName);
     }
